a98e8042e23870b205266598dd82bb191872498e
Nikita Rushmanov <rush3nik@gmail.com>
2022-05-10 18:46:49 -0700

    Deserialization

--- main.rs
+++ main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::io;
+use serde::Deserialize;
+use rust_decimal::Decimal;
+
+#[derive(Debug, Deserialize)]
+struct Transaction {
+    #[serde(rename = "type")]
+    transaction_type: String,
+    client: u16,
+    tx: u32,
+    amount: Decimal,
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut rdr = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path("transactions.csv")?;
+
+    for result in rdr.deserialize() {
+
+        let transaction: Transaction = result?;
+        println!("{:?}", transaction);
+    }
+    Ok(())
 }
